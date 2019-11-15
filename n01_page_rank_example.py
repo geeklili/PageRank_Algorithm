395710bab84964d27860141961e4082fcfe7d6a4
unknown <shicheng.li@pashr.com>
2019-11-15 09:36:08 +0800

    add file

--- n01_page_rank_example.py
+++ n01_page_rank_example.py
@@ -5,24 +5,28 @@ M = [[0, 0, 0],
      [0, 1, 1]]
 U = [1 / 3, 1 / 3, 1 / 3]
 
+# 初始矩阵
 U0 = np.array(U)
+# 转移概率矩阵
+M = np.array(M)
 
-print('The alpha parameter is not used in the formula...')
+print(M.T)
+print('公式中没有加阻尼系数alpha...')
 U_past_none_alpha = []
 while True:
-    U = np.dot(M, U)
-    # print('Un: ', U)
-    if str(U) == str(U_past_none_alpha):
-        break
-    U_past_none_alpha = U
-print('Un converge to: ', U)
+	U = np.dot(M, U)
+	print('Un: ', U)
+	if str(U) == str(U_past_none_alpha):
+		break
+	U_past_none_alpha = U
+print('收敛: ', U)
 
-print('The alpha parameter is not used in the formula, and the value of alpha is 0.85...')
+print('公式中加阻尼系数alpha, 令alpha=0.85...')
 U_past_has_alpha = []
 while True:
-    U = 0.85 * (np.dot(M, U)) + 0.15 * U0
-    # print('Un: ', U)
-    if str(U) == str(U_past_has_alpha):
-        break
-    U_past_has_alpha = U
-print('Un converge to: ', U)
+	U = 0.85 * (np.dot(M, U)) + 0.15 * U0
+	# print('Un: ', U)
+	if str(U) == str(U_past_has_alpha):
+		break
+	U_past_has_alpha = U
+print('收敛: ', U)
